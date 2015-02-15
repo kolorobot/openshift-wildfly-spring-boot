3cc88658a32efa9bf6198f594a76dc31e43fe726
kolorobot <rafal.borowiec@gmail.com>
2015-02-15 23:36:47 +0100

    1

--- src/main/java/demo/people/PeopleRepository.java
+++ src/main/java/demo/people/PeopleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-    @Repository
-    public interface PeopleRepository extends PagingAndSortingRepository<Person, Integer> {
-        List<Person> findByLastName(@Param("lastName") String lastName);
-    }
+@Repository
+public interface PeopleRepository extends PagingAndSortingRepository<Person, Integer> {
+    List<Person> findByLastName(@Param("lastName") String lastName);
+}
