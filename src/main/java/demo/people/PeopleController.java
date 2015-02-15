c7dc7e728ede56c7f878f5878d7934b4a1b315a2
kolorobot <rafal.borowiec@gmail.com>
2015-02-16 00:34:45 +0100

    Revert "Java 7"
    This reverts commit b93c7d96704302dbe5f89a4a9b47a9ef65fc0a93.

--- src/main/java/demo/people/PeopleController.java
+++ src/main/java/demo/people/PeopleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("people")
@@ -19,9 +20,9 @@ public class PeopleController {
 
 
     @RequestMapping
-    public Iterable<Person> findAll(@RequestParam(required = false) String lastName) {
-        if (lastName != null) {
-            return peopleRepository.findByLastName(lastName);
+    public Iterable<Person> findAll(@RequestParam Optional<String> lastName) {
+        if (lastName.isPresent()) {
+            return peopleRepository.findByLastName(lastName.get());
         }
         return peopleRepository.findAll();
     }
