294af2547473e848117b4b1f72f89f24c290a6a2
kolorobot <rafal.borowiec@gmail.com>
2015-02-19 22:39:06 +0100

    Minor changes.

--- src/main/java/demo/people/PeopleController.java
+++ src/main/java/demo/people/PeopleController.java
@@ -1,5 +1,7 @@
 package demo.people;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import java.util.Optional;
 @RestController
 @RequestMapping("people")
 public class PeopleController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PeopleController.class);
 
     private final PeopleRepository peopleRepository;
 
@@ -22,8 +26,10 @@ public class PeopleController {
     @RequestMapping
     public Iterable<Person> findAll(@RequestParam Optional<String> lastName) {
         if (lastName.isPresent()) {
+            LOG.info("Searching by lastName ...");
             return peopleRepository.findByLastName(lastName.get());
         }
+        LOG.info("Searching for all ...");
         return peopleRepository.findAll();
     }
 }
